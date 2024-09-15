8ca19efe4f285fd05c47a8e0ca79d99b41cc056b
Zoofear <misterz0312@gmail.com>
2024-09-15 19:43:30 -0400

    Reformatted to break up notes and code

--- C++ Basics Programming Project/C++ Basics Programming Project.cpp
+++ C++ Basics Programming Project/C++ Basics Programming Project.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int main()
 {
 	//Input Variables
-	double tank = 30;
-	double citympg = 23.5;
-	double highwaympg = 28.9;
+		double tank = 30;
+		double citympg = 23.5;
+		double highwaympg = 28.9;
 
 	//Calculations to create output variabless
-	double citymiles = tank * citympg;
-	double highwaymiles = tank * highwaympg;
+		double citymiles = tank * citympg;
+		double highwaymiles = tank * highwaympg;
 
 	//Display of the output variables
-	cout << "When driving in the city you can drive " << citymiles << " miles far" << endl;
-	cout << "When drivinng in the highway you can drive " << highwaymiles << " miles far" << endl;
+		cout << "When driving in the city you can drive " << citymiles << " miles far" << endl;
+		cout << "When drivinng in the highway you can drive " << highwaymiles << " miles far" << endl;
 
 	return 0;
 }
